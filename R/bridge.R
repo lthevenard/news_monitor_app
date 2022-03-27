fd399e4e8ad1b1c366744cef23c8c9e1024feb30
Lucas Thevenard <lthevenard@gmail.br>
2022-03-26 22:46:29 -0300

    Alpha version of the app.

---
+++ R/bridge.R
@@ -0,0 +1,114 @@
+sources <- c("Notícias das agências")
+
+agencias <- c("ANA", "Anac", "Anatel", "Ancine", "Aneel", "ANM", 
+              "ANP", "ANPD", "ANS", "Antaq", "ANTT", "Anvisa", "CVM")
+
+options <- ""
+
+DT_text_translations <- list(
+    decimal = ",",
+    emptyTable = "Sem dados disponíveis na tabela",
+    info = "Exibindo _START_ a _END_ de _TOTAL_ entradas",
+    infoEmpty = "Exibindo 0 a 0 de 0 entradas",
+    infoFiltered = "(resultados filtrados de um total de _MAX_ entradas)",
+    infoPostFix = "",
+    thousands = ".",
+    lengthMenu = "Exibir _MENU_ entradas",
+    loadingRecords = "Carregando...",
+    processing = "Processando...",
+    search = "Buscar:",
+    zeroRecords = "Não foram encontrados registros correspondentes",
+    paginate = list(
+      first = "Primeira",
+      last =  "Última",
+      `next` =  "Próxima",
+      previous = "Anterior"
+    ),
+    aria = list(
+      sortAscending = ": ativar ordenação ascendente da coluna",
+      sortDescending = ": ativar ordenação descendente da coluna"
+    )
+)
+
+inform_text <- "<p style = 'margin-bottom: 10px; line-height: 150%;'><i>Selecione o ID da notícia desejada (apenas o número) e clique no botão 'Ler notícia selecionada' para visualizar seu texto abaixo.</i></p>"
+inform_table <- "<p style = 'margin-bottom: 10px; line-height: 150%;'><i>Clique em uma linha da tabela abaixo para selecionar o ID da notícia que deseja ler. Em seguida, clique no botão 'Ler notícia selecionada' para visualizar seu texto na aba ao lado.</i></p>"
+
+
+restrict_options_from_source <- function(source) {
+  if (source == sources[[1]]) {
+    return(c("Todas as agências", agencias))
+  }
+}
+
+describe_ref <- function(first_date, last_update) {
+  list(
+    first_date = paste0("<p><b>Monitoramento desde</b>: ", first_date, "</p>"),
+    last_update = paste0("<p><b>Última atualização em</b>: ", last_update, "</p>")
+  )
+}
+
+table_selection <- function(table, input_source, input_filter, input_date) {
+  if (input_source == sources[1]) {
+    table_selection_agencias(table, input_filter, input_date)
+  }
+}
+
+prepare_text <- function(text, title, url) {
+  
+  wrap_paragraph <- function(text) {
+    return(
+      paste0("<p style = 'margin-bottom: 10px; line-height: 150%;'>", text, "</p>")
+    )
+  }
+  
+  paragraphs <- text %>%
+    str_split("[\\n\\r]+") %>% 
+    flatten() %>%
+    map_chr(wrap_paragraph)
+  
+  div <- paste(
+    "<div style = 'font: Verdana;'>", paragraphs, "</div>", collapse = "</br></br>"
+  )
+  
+  link <- paste0(
+    "<p><i>Para acessar a notícia no site da agência, <a href= '",
+    url, "'>clique aqui</a>.</i></p>"
+  )
+  
+  return(paste0(h2(title), "</br>", link, hr(), "</br>", div))
+}
+
+table_selection_agencias <- function(tables, input_filter, input_date) {
+  table <- tables[["agencias"]]
+  cols <- c("id", "agencia", "date", "title", "description")
+  new_column_names <- c("ID", "Agência", "Data de Publicação", "Título", "Chamada")
+  output_table <- table %>% select(all_of(cols)) %>% filter(date >= input_date)
+  names(output_table) <- new_column_names
+  if (input_filter == "Todas as agências") {
+    return(
+      output_table
+    )
+  } else {
+    return(
+      output_table %>% filter(`Agência` == input_filter)
+    )
+  }
+}
+
+condense_references <- function(ids, agencias, datas) {
+  condense_one_ref <- function(id, agencia, data) {
+    paste(
+      "<b>ID</b>:", id, "</br>", agencia, "</br>", data
+    )
+  }
+  return(pmap_chr(list(ids, agencias, datas), condense_one_ref))
+}
+
+column_styles <- function(table) {
+  table %>% 
+    mutate(`Título` = paste0("<b>", `Título`, "</b>"),
+           Chamada = paste0("<i>", `Chamada`, "</i>"),
+           `Referências` = condense_references(ID, `Agência`, `Data de Publicação`)) %>% 
+    select(all_of(c("Referências", "Título", "Chamada")))
+}
+
---
+++ app.R
@@ -0,0 +1,103 @@
+library(shiny)
+library(tidyverse)
+library(lubridate)
+library(DT)
+library(bslib)
+library(emo)
+
+ref <- readRDS("ref.rds")
+news <- readRDS("news.rds")
+
+first_date <- ref$first_date_available
+last_update <- ref$last_update
+ref_descriptions <- describe_ref(first_date, last_update)
+min_date <- dmy(first_date)
+max_date <- dmy(last_update)
+
+
+ui <- fluidPage(
+    theme = bs_theme(bootswatch = "journal", primary = "#183d7a", secondary = "#3f8ccb", info = "#50327c"),
+    titlePanel(NULL, windowTitle = paste(emo::ji("newspaper"), "RegNotícias")),
+    h1(style = "line-height:150%; color:#183d7a;", "RegNotícias - Monitor de notícias sobre regulação"),
+    hr(), br(),
+    
+    sidebarLayout(
+        sidebarPanel(
+          h3(style = "line-height:150%; color:#051d45;", "Selecionar notícias"),
+          selectInput("source", "Fonte:", choices = sources, selected = sources[1]),
+          selectInput("filter", "Opção:", choices = options, selected = options[1]),
+          dateInput("cut_date", "Visualizar notícias a partir de:", value = min_date, min = min_date, max = max_date),
+          br(),
+          
+          h3(style = "line-height:150%; color:#051d45;", "Ler notícia"), 
+          textInput("id_selected", "Informe o ID da notícia que deseja ler", value = news$agencias$id[[nrow(news$agencias)]]),
+          actionButton("go", "Ler notícia selecionada"),
+          
+          br(), hr(), br(), 
+          HTML(ref_descriptions$first_date),
+          HTML(ref_descriptions$last_update)
+        ),
+
+        mainPanel(
+          tabsetPanel(id = "inTabset",
+            tabPanel(title = paste(emo::ji("newspaper"), "Explorar notícias"), 
+                     value = "explorar",
+                     br(), HTML(inform_table),   
+                     h2(style = "line-height:150%; color:#051d45;", "Notícias"), hr(), 
+                     dataTableOutput("news_table"), br(), br()),
+            tabPanel(title = paste(emo::ji("face_with_monocle"), "Ler notícia selecionada"), 
+                     value = "ler",
+                     br(), HTML(inform_text), br(), hr(), 
+                     br(), htmlOutput("news_text"), br(), br(), 
+                     actionButton("back", "Voltar para a tabela de notícias"), br(), br(), br())
+          )
+        )
+    )
+)
+
+
+server <- function(input, output, session) {
+  observeEvent({input$source}, {
+    temp_options <- restrict_options_from_source(input$source)
+    updateSelectInput(session, "filter", choices = temp_options, selected = temp_options[1])
+  })
+  
+  generate_table <- reactive({
+    news %>% 
+      table_selection(input$source, input$filter, input$cut_date)
+  })
+  
+  output$news_table <- renderDataTable(
+    generate_table() %>% 
+      column_styles() %>% 
+      datatable(selection = 'single', rownames = FALSE, escape = FALSE, options = list(language = DT_text_translations))
+  )
+  
+  observeEvent({input$news_table_rows_selected}, {
+    index <- input$news_table_rows_selected[[1]]
+    id_updated <- generate_table()$ID[index]
+    updateTextInput(session, "id_selected", value = id_updated)
+  })
+  
+  observeEvent({input$go}, {
+    updateTabsetPanel(session, "inTabset", selected = "ler")
+  })
+  
+  observeEvent({input$back}, {
+    updateTabsetPanel(session, "inTabset", selected = "explorar")
+  })
+  
+  get_news_texts <- eventReactive({input$go}, {
+    id_selected <- input$id_selected %>% as.numeric()
+    news_row <- bind_rows(news) %>% filter(id == id_selected)
+    return(list(article = news_row$article[[1]], title = news_row$title[[1]], url = news_row$url[[1]]))
+  })
+  
+  output$news_text <- renderUI({
+    texts <- get_news_texts()
+    prepare_text(texts$article, texts$title, texts$url) %>% HTML()
+  }) 
+}
+
+
+shinyApp(ui = ui, server = server)
